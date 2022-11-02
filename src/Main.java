8d85bafaec5ec10039bb558e3dc34248a2bf0bc7
Dima Vasyutin <Vasyutin_dima12@mail.ru>
2022-11-02 11:56:49 +0300

    Add credit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService calculate = new CreditPaymentService();
+        int time = 12;
+        int sumCredit = 1000000;
+        double rate = 9.99;
+        double creditPayment = calculate.credit(time,sumCredit,rate);
+        System.out.println(creditPayment);
+    }
+}
