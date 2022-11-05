256a9fccaade3261247fbcaf6039a432f0c8562a
DimaVasyutin <115107603+DimaVasyutin@users.noreply.github.com>
2022-11-05 13:27:57 +0300

    reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,13 +1,11 @@
-
 public class CreditPaymentService {
-    public double credit(int a,  int b, double c){
+    public double credit(int a, int b, double c) {
         //double i = (Math.pow(1.00 + c/100D, 1/12D) - 1); - есть такой вариант расчета помесячного процента
-        double i = (c/100)/12;
-        double k = (i * Math.pow(i+1,a)) / (Math.pow(i+1,a)-1);
+        double i = (c / 100) / 12;
+        double k = (i * Math.pow(i + 1, a)) / (Math.pow(i + 1, a) - 1);
 
 
-
-        int payment = (int)(b * k);
+        int payment = (int) (b * k);
         return payment;
 
     }
