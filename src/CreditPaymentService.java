3448714b3570cb7fe1485d070076edfc15635c0b
DimaVasyutin <115107603+DimaVasyutin@users.noreply.github.com>
2022-11-06 19:27:04 +0300

    Rename annuityRatio

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -3,9 +3,9 @@ public class CreditPaymentService {
     public double credit(int timeCredit, int sumCredit, double rateCredit) {
         //double i = (Math.pow(1.00 + c/100D, 1/12D) - 1); - есть такой вариант расчета помесячного процента
         double monthlyRate = (rateCredit / 100) / 12; // помесячный процент
-        double k = (monthlyRate * Math.pow(monthlyRate + 1, timeCredit)) / (Math.pow(monthlyRate + 1, timeCredit) - 1);
+        double annuityRatio = (monthlyRate * Math.pow(monthlyRate + 1, timeCredit)) / (Math.pow(monthlyRate + 1, timeCredit) - 1);
 
-        int payment = (int) (sumCredit * k);
+        int payment = (int) (sumCredit * annuityRatio);
         return payment;
 
     }
