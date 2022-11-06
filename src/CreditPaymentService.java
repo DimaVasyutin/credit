ae30619cc9552481e534dde4194e1eebaf130770
DimaVasyutin <115107603+DimaVasyutin@users.noreply.github.com>
2022-11-06 19:22:25 +0300

    Rename var

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,11 @@
+
 public class CreditPaymentService {
-    public double credit(int a, int b, double c) {
+    public double credit(int timeCredit, int sumCredit, double rateCredit) {
         //double i = (Math.pow(1.00 + c/100D, 1/12D) - 1); - есть такой вариант расчета помесячного процента
-        double i = (c / 100) / 12;
-        double k = (i * Math.pow(i + 1, a)) / (Math.pow(i + 1, a) - 1);
+        double monthlyRate = (rateCredit / 100) / 12; // помесячный процент
+        double k = (monthlyRate * Math.pow(monthlyRate + 1, timeCredit)) / (Math.pow(monthlyRate + 1, timeCredit) - 1);
 
-
-        int payment = (int) (b * k);
+        int payment = (int) (sumCredit * k);
         return payment;
 
     }
